65cccb8ab852eb5a42913d492fee1f528f6a26ab
moshefidds <150623725+moshefidds@users.noreply.github.com>
2024-02-11 16:47:58 -0500

    Update 1 table.sql
    Added planet

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -4,6 +4,7 @@ drop table if exists body
 go
 create table dbo.Body(
     id int not null identity primary key, 
+    Planet varchar(50) not null,
     bodypart varchar(100) not null, 
     num int null
     )
